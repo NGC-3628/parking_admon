5da1a1964cf497cf67480ed2fc6deface08079f9
NGC-3628 <isaacvgn@gmail.com>
2026-02-22 23:12:36 -0600

    ya funciona payment

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def menu():
         print("\n==============================")
         print("   SISTEMA DE ESTACIONAMIENTO   ")
         print("==============================")
-        print("1. Ingresar Vehículo (Entrance)")
-        print("2. Pagar Ticket (Payment)")
-        print("3. Salir Vehículo (Exit)")
+        print("1. Ingresar Vehículo (Entrance) ")
+        print("2. Pagar Ticket (Payment) ")
+        print("3. Salir Vehículo (Exit) ")
         print("4. Cerrar Sistema")
         
         opcion = input("Seleccione una opción: ")
@@ -18,7 +18,7 @@ def menu():
         if opcion == '1':
             entrance.ejecutar_entrada()
         elif opcion == '2':
-            payment.ejecutar_pago()
+            payment.maquinaPago()
         elif opcion == '3':
             exit.ejecutar_salida()
         elif opcion == '4':
--- payment.py
+++ payment.py
@@ -1,51 +1,76 @@
 import data_generator as dg
 from datetime import datetime
 
+exit_message = "\nGracias por su pago"
 
-def calcular_pago(tiempo_transcurrido):
-    if tiempo_transcurrido < 43000:  
-        return 25 
-    elif tiempo_transcurrido > 43000:  # 12 horas
-        return 50
-    elif tiempo_transcurrido > 86400:  # hasta 24 horas
-        return 100
-    elif tiempo_transcurrido > 172800:  # hasta 48 horas
-        return 200
-
-def ejecutar_pago():
-    print("\n--- MAQUINA DE PAGO ---")
-    plate = input ("Ingrese su numero de placa:").upper()
-
-    registro = dg.buscar_placa_activa(plate)
+####### MENU DE CAJA AUTOMATICA ######
+def maquinaPago():
+    print("\n --- MAQUINA DE PAGO ---")
+    plate = input("Ingrese su placa por favor ").upper()
+    registro = validar_placa(plate)
 
     if registro:
-        hora_entrada = registro['hora_entrada']
-        ahora = datetime.now()
-        tiempo_transcurrido = (ahora - hora_entrada).total_seconds()
-
-        print(f"Tiempo transcurrido: {int(tiempo_transcurrido / 60)} minutos")
-        a_pagar = calcular_pago(tiempo_transcurrido)
-        dinero = int(input(f"Por favor ingrese ${a_pagar}: "))
-
-
-        # validar que pago sea suficiente
-        while dinero < a_pagar:
-            print(f"por favor ingrese mas dinero. Faltan ${a_pagar - dinero} pesos")
-            dinero = int(input(f"Por favor ingrese ${a_pagar}: "))
-
-
-        #cambio 
-        if dinero > a_pagar:
-            print(f"Su cambio es ${dinero - a_pagar}")
-
-        # registrar pago solo si se ha pagado el monto correcto
-        if dinero == a_pagar:
-            deuda_total = dinero
-
-            # registrar pago
-            if deuda_total == a_pagar:
-                dg.registrar_pago_db(registro['id'])
-
+        ejecutar_pago(registro)
     else:
         print("Placa no encontrada. Intente nuevamente.")
+        maquinaPago()
+    
+
+####### VALIDACION DE PLACA ######
+def validar_placa (plate):
+    registro = dg.buscar_placa_activa(plate)
+    if registro:
+        return registro
+    else:
+        return False
+
+####### calculo de cobro ######
+def calcular_pago(tiempo_transcurrido):
+    if tiempo_transcurrido > 172800:  # > 48 horas
+        return 200
+    elif tiempo_transcurrido > 86400:  # > 24 horas
+        return 100
+    elif tiempo_transcurrido >= 43000: # > 12 horas
+        return 50
+    else:                              #<= 12 horas
+        return 25
+
+####### EJECUCION DE PAGO ######
+def ejecutar_pago(registro):
+
+    if registro["estado"] == 1:
+        print("Este boleto ya esta pagado")
+        return
+
+    hora_entrada = registro['hora_entrada']
+    ahora = datetime.now()
+    tiempo_transcurrido = (ahora - hora_entrada).total_seconds()
+
+    print(f"Tiempo transcurrido: {int(tiempo_transcurrido / 60)} minutos")
+
+    a_pagar = calcular_pago(tiempo_transcurrido)
+    ingreso = int(input(f"Por favor ingrese ${a_pagar} "))
+
+    
+    if ingreso == a_pagar:
+        pass
+    
+    elif ingreso > a_pagar:
+        print(f"Entregando cambio. \nSu cambio es ${ingreso - a_pagar}")
+    
+    else:
+        falta = a_pagar - ingreso
+        while falta > 0:
+            print(f"Saldo insuficiente. Le falta ${falta}")
+            nuevo_ingreso = int(input("Ingrese el dinero restante: "))
+            falta = falta - nuevo_ingreso
+
+            if falta < 0:
+                print(f"Entregando cambio. \nSu cambio es ${abs(falta)}")
+            
+    dg.registrar_pago_db(registro['id'])
+    
+    print(exit_message)
+
+
 
